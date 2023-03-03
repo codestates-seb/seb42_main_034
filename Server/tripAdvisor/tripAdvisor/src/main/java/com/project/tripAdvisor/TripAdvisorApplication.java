66114afd61ab65b29d2fd00424d6623b6927b12a
JungYeon222 <dlwjddus16@naver.com>
2023-03-03 19:04:31 +0900

    first commit

---
+++ Server/tripAdvisor/tripAdvisor/src/main/java/com/project/tripAdvisor/TripAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.project.tripAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripAdvisorApplication.class, args);
+	}
+
+}
---
+++ Server/tripAdvisor/tripAdvisor/src/test/java/com/project/tripAdvisor/TripAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tripAdvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
