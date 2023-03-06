b01a163ff9c1f9c24b7131bd075dcf2eae142f0c
JungYeon222 <dlwjddus16@naver.com>
2023-03-06 16:29:40 +0900

    init

--- Server/tripAdvisor/tripAdvisor/src/main/java/com/project/tripAdvisor/TripAdvisorApplication.java
+++ Server/tripAdvisor/tripAdvisor/src/main/java/com/project/tripAdvisor/TripAdvisorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TripAdvisorApplication {
+public class TripAdvisorApplication {//
 
 	public static void main(String[] args) {
 		SpringApplication.run(TripAdvisorApplication.class, args);
