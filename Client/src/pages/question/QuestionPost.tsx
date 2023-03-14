46c11b4e842bc6aa9eca916fe523c46e438ee3a9
skuxyx <skubaltz@naver.com>
2023-03-14 23:11:41 +0900

    [feat] 게시글 기능 구현 ( 진행중 )

--- Client/src/pages/question/QuestionPost.tsx
+++ Client/src/pages/question/QuestionPost.tsx
@@ -1,5 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Editor from '../../component/board/Editor';
+
+// css
+
+const Input = styled.input`
+  display: flex;
+`;
 
 export default function QuestionPost() {
-  return <div>QuestionPost</div>
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+
+  const titleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
+
+  const contentHalder = (value: string) => {
+    setContent(value);
+  };
+
+  const handleSubmit = () => {
+    // axios
+  };
+
+  return (
+    <>
+      <Input type="text" value={title} onChange={titleHandler} />
+      <Editor value={content} onChange={contentHalder} />
+      <button onClick={handleSubmit}>작성</button>
+    </>
+  );
 }
