74dd18bcddc5861037c2f23cff67ce13cb2eab98
skuxyx <skubaltz@naver.com>
2023-03-16 09:33:17 +0900

    [Feat] 게시글 리스트 컴포넌트, 게시글 작성 페이지 구현

--- Client/src/pages/question/QuestionPost.tsx
+++ Client/src/pages/question/QuestionPost.tsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { ButtonWrapper } from '../../component/board/Button';
 import Editor from '../../component/board/Editor';
 
 // css
+const PostWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Button = styled(ButtonWrapper)`
+  margin-top: 10px;
+`;
 
 const Input = styled.input`
   display: flex;
+  width: 40rem;
+  height: 25px;
+  border: 1px solid var(--main-001);
+  border-radius: 5px;
+  :focus {
+    outline: 2px solid var(--main-002);
+  }
 `;
 
 export default function QuestionPost() {
@@ -16,19 +35,30 @@ export default function QuestionPost() {
     setTitle(e.target.value);
   };
 
-  const contentHalder = (value: string) => {
+  const contentHandler = (value: string) => {
     setContent(value);
   };
 
-  const handleSubmit = () => {
+  const submitHandler = () => {
+    if (title.trim() === '') {
+      alert('제목을 입력하세요.');
+      return;
+    }
+    if (content.trim() === '') {
+      alert('내용을 입력하세요.');
+      return;
+    }
+    window.confirm('작성하시겠습니까?');
     // axios
   };
 
   return (
     <>
-      <Input type="text" value={title} onChange={titleHandler} />
-      <Editor value={content} onChange={contentHalder} />
-      <button onClick={handleSubmit}>작성</button>
+      <PostWrapper>
+        <Input type="text" value={title} onChange={titleHandler} placeholder="제목" />
+        <Editor value={content} onChange={contentHandler} />
+        <Button onClick={submitHandler}>작성</Button>
+      </PostWrapper>
     </>
   );
 }
