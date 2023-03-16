e34c1a79b906e75a68d5cfda8319bb40e31e5614
sujin <sjij0617@naver.com>
2023-03-16 21:31:57 +0900

    [fix]일부오류수정

--- Client/src/redux/loginInfoReducer.ts
+++ Client/src/redux/loginInfoReducer.ts
@@ -1,42 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 interface InitProps {
-    [key: string]: any;
-    memberId: number;
-    nickname: string;
-    location: {
-      latitude:string;
-      longitude:string;
-    };
-    address: string;
-    totalCount: number;
-      avatarUrl:string;
-  }
-  
-  const initialState: InitProps = {
-    memberId: 0,
-    nickname: '',
-    location: {
-      latitude:"",
-      longitude:""
+  [key: string]: any;
+  memberId: number;
+  nickname: string;
+  location: {
+    latitude: string;
+    longitude: string;
+  };
+  address: string;
+  totalCount: number;
+  avatarUrl: string;
+}
+
+const initialState: InitProps = {
+  memberId: 0,
+  nickname: '',
+  location: {
+    latitude: '',
+    longitude: '',
+  },
+  address: '',
+  totalCount: 0,
+  avatarUrl: '',
+  avgGrade: 0,
+};
+
+const userInfoSlice = createSlice({
+  name: 'userInfo',
+  initialState,
+  reducers: {
+    setUserInfo: (_, action) => action.payload,
+    updateUserInfo: (state, action) => {
+      const { key } = action.payload;
+      state[key] = action.payload.value;
     },
-    address: '',
-    totalCount: 0,
-      avatarUrl:"",
-    avgGrade: 0
-  }
-  
-  const userInfoSlice = createSlice({
-    name: 'userInfo',
-    initialState,
-    reducers: {
-      setUserInfo: (_, action) => action.payload,
-      updateUserInfo: (state, action) => {
-        const {key} = action.payload;
-        state[key] = action.payload.value;
-      }
-    }
-  })
-  
-  export const {setUserInfo, updateUserInfo} = userInfoSlice.actions;
-  export default userInfoSlice.reducer; 
+  },
+});
+
+export const { setUserInfo, updateUserInfo } = userInfoSlice.actions;
+export default userInfoSlice.reducer;
--- Client/src/redux/userInfoReducer.ts
+++ Client/src/redux/userInfoReducer.ts
@@ -1,41 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 interface InitProps {
-    [key: string]: any;
-    memberId: number;
-    nickname: string;
-    location: {
-        latitude: string;
-        longitude: string;
-    };
-    address: string;
-    totalCount: number;
-    avatarUrl: string;
+  [key: string]: any;
+  memberId: number;
+  nickname: string;
+  location: {
+    latitude: string;
+    longitude: string;
+  };
+  address: string;
+  totalCount: number;
+  avatarUrl: string;
 }
 
 const initialState: InitProps = {
-    memberId: 0,
-    nickname: '',
-    location: {
-        latitude:"",
-        longitude:""
-    },
-    address:'',
-    totalCount: 0,
-    avatarUrl:"",
-}
+  memberId: 0,
+  nickname: '',
+  location: {
+    latitude: '',
+    longitude: '',
+  },
+  address: '',
+  totalCount: 0,
+  avatarUrl: '',
+};
 
 const userInfoSlice = createSlice({
-    name: 'userInfo',
-    initialState,
-    reducers: {
-        setUserInfo:(_, action) => action.payload,
-        updateUserInfo: (state, action) => {
-            const {key} = action.payload;
-            state[key] = action.payload.value;
-        }
-    }
-})
+  name: 'userInfo',
+  initialState,
+  reducers: {
+    setUserInfo: (_, action) => action.payload,
+    updateUserInfo: (state, action) => {
+      const { key } = action.payload;
+      state[key] = action.payload.value;
+    },
+  },
+});
 
-export const {setUserInfo, updateUserInfo} = userInfoSlice.actions;
+export const { setUserInfo, updateUserInfo } = userInfoSlice.actions;
 export default userInfoSlice.reducer;
