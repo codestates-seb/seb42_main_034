0c07229bc6bc86865e0bf6d5b37e4613dd13957f
gwoongchoi <opo1220@gmail.com>
2023-03-16 20:03:04 +0900

    FE 버튼,인풋 컴포넌트, Blogpost 작성

--- Client/src/component/style/styled.d.ts
+++ Client/src/component/style/styled.d.ts
@@ -2,8 +2,32 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    media;
+    colors: {
+      main1: string;
+      main2: string;
+      main3: string;
+      button: string;
+      buttonHover: string;
+      buttonClicked: string;
+      warn: string;
+      black: string;
+      grey: string;
+      white: string;
+      green: string;
+      border1: string;
+      border2: string;
+      border3: string;
+    };
+    font: {
+      small: string;
+      base: string;
+      large: string;
+      h1: string;
+      h2: string;
+      h3: string;
+    };
   }
+  // media;
 }
 declare module 'react' {
   interface Attributes {
