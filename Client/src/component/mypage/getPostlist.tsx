a6573e78ab19cfd9198d2347db4980455584b922
myongdol <myongdol132@gmail.com>
2023-04-18 15:27:42 +0900

    [test] 유저가 작성한 글목록 불러오기 테스트

--- Client/src/component/mypage/getPostlist.tsx
+++ Client/src/component/mypage/getPostlist.tsx
@@ -1,10 +1,19 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import styled from "styled-components";
 import { useQuery } from "@tanstack/react-query";
 import { useMypageAPI } from "api/mypage";
 import { useAppSelector } from "redux/hooks";
+import { useParams } from "react-router-dom";
+import AxiosCustom from "utils/AxiosCustom";
+import useAPI from "hooks/uesAPI";
 
-const PostList: FunctionComponent = () => {
+interface Post {
+  content: string;
+  id : number;
+}
+
+
+const PostList = () => {
   const { id } = useAppSelector((state) => state.loginInfo);
   const { getMyInfo } = useMypageAPI();
   const { data } = useQuery({
@@ -12,20 +21,50 @@ const PostList: FunctionComponent = () => {
     queryFn: () => getMyInfo(id),
     retry: false,
   });
+// console.log(data)
+ const api = useAPI();
+
+  const { memberId} = useParams();
+  const [post, setPost] = useState<Post[]|[]>([]); 
+  
+  const getPost = async () => {
+    await api
+    .get(`/members/${memberId}/posts`)
+    .then(resp => {
+      setPost(resp.data.data);
+
+    })
+    .catch(error => {
+      console.log(error);
+    }) 
+  }
+
+  useEffect(() => {
+    getPost();
+  }, [memberId])
+
+
+console.log(post)                               
 
   return (
     <MainContainer>
       {data?.nickname}
       {data?.comments}
+      {data?.email}
+      {post.map((p) => (
+      <div key={p.id}>
+        <p>{p.content}</p>
+      </div>
+    ))}
     </MainContainer>
   );
 };
 
 const MainContainer = styled.div`
-  width: 60%;
+  width: 100%;
   display: flex;
   justify-content: space-between;
-  border: 1px solid #eaeaea;
+  border: 2px solid red;
 `;
 
 export default PostList;
--- Client/src/pages/MyPage.tsx
+++ Client/src/pages/MyPage.tsx
@@ -75,7 +75,7 @@ export default function MyPage() {
       <UserInfoContainer>
         <p>닉네임: {data?.nickname ?? '로그인 정보를 불러오지 못했습니다.'}</p>
         <p>
-          도시 : {data?.address ?? '도시가 설정되어 있지 않습니다.'}
+          도시 : {data?.location ?? '도시가 설정되어 있지 않습니다.'}
         </p>
         <div className='editprofile'>
           <p className='editbtn' onClick={linkEditPage}>
