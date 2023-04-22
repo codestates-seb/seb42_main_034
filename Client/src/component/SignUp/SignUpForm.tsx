7e0fe7c3f011111b474cded3c7f1574f4ae830c6
myongdol <myongdol132@gmail.com>
2023-04-22 17:03:26 +0900

    [test] 커밋테스트 겸 백업

--- Client/src/component/SignUp/SignUpForm.tsx
+++ Client/src/component/SignUp/SignUpForm.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Nbutton from 'component/ui/NButton';
-import { notifi } from '../../utils/notifi';
+// import { notifi } from '../../utils/notifi';
 import styled from 'styled-components';
 import React from 'react';
 import { keyframes } from 'styled-components';
@@ -38,11 +38,11 @@ export const SignUpForm = () => {
 
       if (!emailRegex.test(emailCurrent)) {
         setEmailMessage('이메일 형식이 올바르지 않습니다.');
-        notifi(dispatch, '이메일 형식이 올바르지 않습니다.');
+        // notifi(dispatch, '이메일 형식이 올바르지 않습니다.');
         setIsEmail(false);
       } else {
         setEmailMessage('올바른 이메일 형식입니다.');
-        notifi(dispatch, '올바른 이메일 형식입니다.');
+        // notifi(dispatch, '올바른 이메일 형식입니다.');
         setIsEmail(true);
       }
     },
@@ -57,7 +57,7 @@ export const SignUpForm = () => {
     setNickname(event.target.value);
     if (event.target.value.length < 2 || event.target.value.length > 10) {
       setNicknameMessage('2글자 이상 10글자 미만으로 입력해주세요.');
-      notifi(dispatch, '올바르지 않은 닉네임 형식입니다.');
+      // notifi(dispatch, '올바르지 않은 닉네임 형식입니다.');
       setIsNickname(false);
     } else {
       setNicknameMessage('');
@@ -69,7 +69,7 @@ export const SignUpForm = () => {
  // 닉네임 중복확인 
  const handleCheckNickname = () => {
   Swal.fire('', '사용 가능한 닉네임입니다.')
-  notifi(dispatch, '사용 가능한 닉네임입니다.')
+  // notifi(dispatch, '사용 가능한 닉네임입니다.')
  }
   // const handleCheckNickname = async () => {
   //   await AxiosCustom
@@ -123,7 +123,7 @@ export const SignUpForm = () => {
         setIsPwCheck(true);
       } else {
         setPwCheckMessage('비밀번호가 다릅니다.');
-        notifi(dispatch, '비밀번호가 일치 하지않습니다')
+        // notifi(dispatch, '비밀번호가 일치 하지않습니다')
         setIsPwCheck(false);
       }
     },
@@ -140,7 +140,7 @@ export const SignUpForm = () => {
       isPwCheck === false
     ) {
       Swal.fire('', '양식을 다시 확인해주세요');
-      notifi(dispatch, '회원가입 양식을 획인 해주세요.')
+      // notifi(dispatch, '회원가입 양식을 획인 해주세요.')
     } else {
       await AxiosCustom
         .post(`/members`, {
